7d3febbed0bd6828ef5df95ab10186db79a50920
Pavel Keyzik <pavel.keyzik@gmail.com>
2022-10-12 21:04:25 +0200

    Add vitest config

---
+++ src/core/tests/randomizer.test.js
@@ -0,0 +1,11 @@
+import { expect, it } from "vitest";
+// Uncomment this line and start to work on this unit test
+// import { buildRandomizer } from '../randomizer'
+
+// TODO: Check that when we generate random message is always unique
+// The logic here is that for example we have 5 messages, we generated one
+// and removed from messages array, until our array will be empty.
+// When it's empty we just replaces this array with the initial array
+it("should always return unique message", () => {
+  expect(true).toBe(true);
+});
