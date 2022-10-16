6b0a1a3b43dab7ac2e9f868b235a1165479c90dd
batunpc <batuhnpc@gmail.com>
2022-10-16 05:09:43 -0400

    tests: should return initial array when the array is empty

--- src/core/tests/randomizer.test.js
+++ src/core/tests/randomizer.test.js
@@ -11,3 +11,14 @@ it("should always return unique message", () => {
   expect(messages.length).toBe(5);
   expect(uniqueMessages.size).toBe(messages.length);
 });
+
+it("should return initial array when the array is empty", () => {
+  const randomizer = buildRandomizer();
+  const messages = Array.from({ length: 20 }, () =>
+    randomizer.getRandomMessage()
+  );
+  const uniqueMessages = new Set(messages);
+
+  expect(messages.length).toBe(20);
+  expect(uniqueMessages.size).toBe(messages.length);
+});
