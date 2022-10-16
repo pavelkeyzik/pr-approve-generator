c753156d4e32503350ef8c79aa01bc94a3d3c422
batunpc <batuhnpc@gmail.com>
2022-10-16 16:20:03 -0400

    fix: randomizer.test static no of elements

--- src/core/randomizer.js
+++ src/core/randomizer.js
@@ -1,8 +1,6 @@
-import { messages } from "./messages";
-
-function buildRandomizer() {
+function buildRandomizer(messages) {
   const state = {
-    currentMessage: messages[0],
+    messages: [...messages],
     messagesState: messages.slice(1),
   };
 
@@ -10,10 +8,8 @@ function buildRandomizer() {
     getRandomMessage() {
       const { messagesState } = state;
       const index = Math.floor(Math.random() * messagesState.length);
-
       let newMessage;
       let newMessagesState;
-
       if (messagesState.length !== 0) {
         newMessage = messagesState[index];
         newMessagesState = [
@@ -27,11 +23,9 @@ function buildRandomizer() {
           ...messages.slice(index + 1),
         ];
       }
-
       state.messagesState = newMessagesState;
       state.currentMessage = newMessage;
-
-      return state.currentMessage;
+      return { newMessage, newMessagesState };
     },
   };
 }
--- src/core/tests/randomizer.test.js
+++ src/core/tests/randomizer.test.js
@@ -2,23 +2,9 @@ import { expect, it } from "vitest";
 import { buildRandomizer } from "../randomizer";
 
 it("should always return unique message", () => {
-  const randomizer = buildRandomizer();
-  const messages = Array.from({ length: 5 }, () =>
-    randomizer.getRandomMessage()
-  );
-  const uniqueMessages = new Set(messages);
+  const messages = ["1", "2", "3", "4", "5"];
+  const randomizer = buildRandomizer(messages);
 
-  expect(messages.length).toBe(5);
-  expect(uniqueMessages.size).toBe(messages.length);
-});
-
-it("should return initial array when the array is empty", () => {
-  const randomizer = buildRandomizer();
-  const messages = Array.from({ length: 20 }, () =>
-    randomizer.getRandomMessage()
-  );
-  const uniqueMessages = new Set(messages);
-
-  expect(messages.length).toBe(20);
-  expect(uniqueMessages.size).toBe(messages.length);
+  const { newMessage, newMessagesState } = randomizer.getRandomMessage();
+  expect(newMessagesState).not.toContain(newMessage);
 });
