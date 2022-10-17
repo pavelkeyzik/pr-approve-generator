4a43dffee57bb0f50f93ce743e80aa656bc6c9cf
batunpc <batuhnpc@gmail.com>
2022-10-16 20:05:53 -0400

    fix: randomizer.test use messageState

--- src/core/tests/randomizer.test.js
+++ src/core/tests/randomizer.test.js
@@ -8,3 +8,14 @@ it("should always return unique message", () => {
   const { newMessage, newMessagesState } = randomizer.getRandomMessage();
   expect(newMessagesState).not.toContain(newMessage);
 });
+
+it("should return initial array when the array is empty", () => {
+  const messages = ["1", "2", "3", "4", "5"];
+  const randomizer = buildRandomizer(messages);
+  for (let i = 0; i < 6; i++) {
+    const { newMessage, newMessagesState } = randomizer.getRandomMessage();
+    expect(newMessagesState).not.toContain(newMessage);
+  }
+  const { newMessage, newMessagesState } = randomizer.getRandomMessage();
+  expect(newMessagesState).not.toContain(newMessage);
+});
