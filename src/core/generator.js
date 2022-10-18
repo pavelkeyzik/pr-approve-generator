94777430dba3f1d017b8297c52ab9eb77688f162
Maciej Kotowski <makoteq@gmail.com>
2022-10-19 00:15:09 +0200

    add emoji changing button

--- src/core/generator.js
+++ src/core/generator.js
@@ -1,23 +1,34 @@
 import { buildRandomizer } from "./randomizer";
-import { messages } from "./messages";
+import { messages, emojis } from "./messages";
 /**
  * Setup generator function that takes some config and adds logic to generate message
  *
  * @param {object} config Configuration for generator
  * @param {string} config.generateButtonSelector Selector for generate new message button
+ * @param {string} config.generateEmojiButtonSelector Selector for generate new emoji button
  * @param {string} config.copyButtonSelector Selector for copy button
  * @param {string} config.resultContainer Selector for container where we put our result message
+ * @param {string} config.resultEmojiContainer Selector for container where we put our result message
  */
 function setupGenerator(config) {
   const generateButton = document.querySelector(config.generateButtonSelector);
+  const generateEmojiButton = document.querySelector(config.generateEmojiButtonSelector);
   const copyButton = document.querySelector(config.copyButtonSelector);
   const output = document.querySelector(config.resultContainer);
+  const emojiOutput = document.querySelector(config.resultEmojiContainer);
   const randomizer = buildRandomizer(messages);
-
+  let index = 0;
   function generateNewMessage() {
     copyButton.classList.remove("btn-copied");
     const { newMessage } = randomizer.getRandomMessage();
-    output.innerText = newMessage; // message
+    output.innerText = newMessage;
+  }
+  function generateNewEmoji() {
+    copyButton.classList.remove("btn-copied");
+    console.log(index);
+    emojiOutput.innerHTML = emojis[index];
+    if (index >= emojis.length - 1) index = 0;
+    else ++index;
   }
 
   function copyMessageToClipboard() {
@@ -27,6 +38,7 @@ function setupGenerator(config) {
 
   // Subscribe to click events
   generateButton.addEventListener("click", generateNewMessage);
+  generateEmojiButton.addEventListener("click", generateNewEmoji);
   copyButton.addEventListener("click", copyMessageToClipboard);
 
   // Just to generate the first message
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { setupGenerator } from "./core/generator";
 
 setupGenerator({
   generateButtonSelector: "#js-generate",
+  generateEmojiButtonSelector: "#js-emoji",
   copyButtonSelector: "#js-copy",
   resultContainer: "#js-result",
+  resultEmojiContainer: "#js-result-emoji",
 });
