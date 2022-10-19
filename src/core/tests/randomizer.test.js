d5387ac65f55b5f74f2f9bcfc65690921e3418e1
Maciej Kotowski <makoteq@gmail.com>
2022-10-19 15:56:15 +0200

    further tests fix

--- src/core/tests/randomizer.test.js
+++ src/core/tests/randomizer.test.js
@@ -3,7 +3,8 @@ import { buildRandomizer } from "../randomizer";
 
 it("should always return unique message", () => {
   const messages = ["1", "2", "3", "4", "5"];
-  const randomizer = buildRandomizer(messages,messages);
+  const emojis = ["1", "2", "3", "4", "5"];
+  const randomizer = buildRandomizer(messages, emojis);
 
   const { newMessage, newMessagesState } = randomizer.getRandomMessage();
   expect(newMessagesState).not.toContain(newMessage);
@@ -11,7 +12,8 @@ it("should always return unique message", () => {
 
 it("should return initial array when the array is empty", () => {
   const messages = ["1", "2", "3", "4", "5"];
-  const randomizer = buildRandomizer(messages,messages);
+  const emojis = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const randomizer = buildRandomizer(messages, emojis);
 
   for (let i = 0; i < messages.length; i++) {
     const { newMessagesState } = randomizer.getRandomMessage();
