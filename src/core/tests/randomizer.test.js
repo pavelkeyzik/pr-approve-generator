acf0030d15e3b185db4a9c8aff9c12c2a7c14538
batunpc <batuhnpc@gmail.com>
2022-10-20 04:57:23 -0400

    codecov fulfilled for randomizer.test

--- src/core/tests/randomizer.test.js
+++ src/core/tests/randomizer.test.js
@@ -1,24 +1,39 @@
-import { expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { buildRandomizer } from "../randomizer";
 
-it("should always return unique message", () => {
-  const messages = ["1", "2", "3", "4", "5"];
-  const emojis = ["1", "2", "3", "4", "5"];
-  const randomizer = buildRandomizer(messages, emojis);
+describe("Rules for generated messages in randomizer", () => {
+  it("should always return unique message", () => {
+    const messages = ["1", "2", "3", "4", "5"];
+    const emojis = ["1", "2", "3", "4", "5"];
+    const randomizer = buildRandomizer(messages, emojis);
 
-  const { newMessage, newMessagesState } = randomizer.getRandomMessage();
-  expect(newMessagesState).not.toContain(newMessage);
-});
+    const { newMessage, newMessagesState } = randomizer.getRandomMessage();
+    expect(newMessagesState).not.toContain(newMessage);
+  });
 
-it("should return initial array when the array is empty", () => {
-  const messages = ["1", "2", "3", "4", "5"];
-  const emojis = ["1", "2", "3", "4", "5"];
-  const randomizer = buildRandomizer(messages, emojis);
+  it("should return initial array when the array is empty", () => {
+    const messages = ["1", "2", "3", "4", "5"];
+    const emojis = ["1", "2", "3", "4", "5"];
+    const randomizer = buildRandomizer(messages, emojis);
 
-  for (let i = 0; i < messages.length; i++) {
-    const { newMessagesState } = randomizer.getRandomMessage();
-    if (i === messages.length - i) {
-      expect(newMessagesState).toEqual(messages);
+    for (let i = 0; i < messages.length; i++) {
+      const { newMessagesState } = randomizer.getRandomMessage();
+      if (i === messages.length - i) {
+        expect(newMessagesState).toEqual(messages);
+      }
     }
-  }
+  });
+
+  it("should return unique emoji when it is triggered", () => {
+    const messages = ["1", "2", "3", "4", "5"];
+    const emojis = ["1", "2", "3", "4", "5"];
+    const randomizer = buildRandomizer(messages, emojis);
+
+    for (let i = 0; i < emojis.length; i++) {
+      const { newEmojiState } = randomizer.getRandomEmoji();
+      if (i === emojis.length - i) {
+        expect(newEmojiState).toEqual(emojis);
+      }
+    }
+  });
 });
